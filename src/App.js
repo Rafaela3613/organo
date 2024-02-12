f06d7f64102bbf52f5a86c3f175005de63d22e7d
Rafaela Barbosa <rafaelabarbosa@MacBook-Pro-de-Rafaela.local>
2024-02-12 15:11:39 -0300

    feat(main): implementa titulo do projeto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bem-vindo ao React Rafaela Biuntal !</p>
+        <h1>Organo</h1>
+        
+        <p>Bem-vindo ao React Rafaela Biuntal!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
