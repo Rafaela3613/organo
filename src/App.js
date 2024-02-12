f7b6be4deb0e88b76d790e2f39e7f7d9da030e0b
Rafaela Barbosa <rafaelabarbosa@MacBook-Pro-de-Rafaela.local>
2024-02-12 15:04:00 -0300

    feat(on01-commitizen): implementa o uso do commitizen no projeto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bem-vindo ao React !</p>
+        <p>Bem-vindo ao React Rafaela Biuntal !</p>
         <a
           className="App-link"
           href="https://reactjs.org"
