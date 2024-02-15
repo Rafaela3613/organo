94d81af1f7c20eebd55dc6b0be73560f9f38c9bb
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-15 13:48:11 -0300

    interagindo com o usuario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,12 +2,17 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`
+
+    const aoDigitado = (evento) => {
+        console.log(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
                 </label>
-                <input placeholder={placeholderModificada}/>
+                <input onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
