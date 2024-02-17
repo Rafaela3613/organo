8885606cb3a7dcc80f066e0b010d7730ad4b2d05
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-17 16:52:27 -0300

    Controlando o estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,13 +1,11 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`
 
-    let valor = 'Rafaela Barbosa'
-
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
 
     return (
@@ -15,7 +13,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
                 </label>
-                <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+                <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
